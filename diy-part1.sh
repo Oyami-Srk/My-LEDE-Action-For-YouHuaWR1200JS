b8f72b5cd250ac699440035556003f5ede2e2c5f
Shiroko <hhx.xxm@gmail.com>
2021-08-28 04:21:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
